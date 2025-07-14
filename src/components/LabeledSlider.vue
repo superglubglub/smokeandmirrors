f1cf8aa4f08d405f3d940dbabc6aeaf3fc72bd26
superglubglub <maxyslater@pm.me>
2025-07-13 19:20:01 -0700

    fixed some random errors

--- src/components/LabeledSlider.vue
+++ src/components/LabeledSlider.vue
@@ -60,7 +60,7 @@ watch(
 }
 .input {
   align-self: stretch;
-  box-shadow: -1px -1px 0px #000 inset, 1px 1px 0px #fff inset, -2px -2px 0px #7f7f7f inset, 2px 2px 0px #dfdfdf inset;
+  box-shadow: -1px -1px #000 inset, 1px 1px #fff inset, -2px -2px  #7f7f7f inset, 2px 2px  #dfdfdf inset;
   background-color: #fff;
   height: 33px;
   display: flex;
@@ -82,54 +82,13 @@ watch(
 .sliderbarInner {
   align-self: stretch;
   flex: 1;
-  box-shadow: -1px -1px 0px #fff inset, 1px 1px 0px #808080 inset;
+  box-shadow: -1px -1px  #fff inset, 1px 1px  #808080 inset;
   background-color: #c0c0c0;
   z-index: 0;
 }
-.rightButtonIcon {
-  width: 25px;
-  position: absolute;
-  margin: 0 !important;
-  top: calc(50% - 12.5px);
-  right: 4px;
-  height: 25px;
-  object-fit: contain;
-  z-index: 1;
-}
-.leftButtonIcon {
-  width: 25px;
-  position: absolute;
-  margin: 0 !important;
-  top: 4px;
-  left: 4px;
-  height: 25px;
-  object-fit: contain;
-  z-index: 2;
-}
-.chevron {
-  position: relative;
-  line-height: 22px;
-}
-.indicatorBlock {
-  width: 24px;
-  margin: 0 !important;
-  position: absolute;
-  top: calc(50% - 12.5px);
-  left: calc(50% - 11.5px);
-  box-shadow: -1px -1px 0px #000 inset, 1px 1px 0px #fff inset, -2px -2px 0px #7f7f7f inset, 2px 2px 0px #dfdfdf inset;
-  background-color: #c0c0c0;
-  height: 25px;
-  overflow: hidden;
-  flex-shrink: 0;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  z-index: 3;
-}
 .w95Slider {
   flex: 1;
-  box-shadow: -1px -1px 0px #000 inset, 1px 1px 0px #fff inset, -2px -2px 0px #7f7f7f inset, 2px 2px 0px #dfdfdf inset;
+  box-shadow: -1px -1px  #000 inset, 1px 1px  #fff inset, -2px -2px  #7f7f7f inset, 2px 2px  #dfdfdf inset;
   background-color: #c0c0c0;
   height: 33px;
   overflow: hidden;
@@ -160,8 +119,6 @@ watch(
 }
 .dragSizeIcon {
   position: absolute;
-  right: 0px;
-  bottom: 0px;
   width: 13px;
   height: 13px;
 }
@@ -169,7 +126,7 @@ watch(
   width: 127px;
   flex: 1;
   position: relative;
-  box-shadow: -1px -1px 0px #fff inset, 1px 1px 0px #808080 inset;
+  box-shadow: -1px -1px  #fff inset, 1px 1px  #808080 inset;
   background-color: #c0c0c0;
   text-align: left;
   font-size: 11px;
@@ -189,12 +146,7 @@ watch(
   text-align: center;
   font-size: 20px;
   color: #000;
-  font-family: W95FA;
-}
-.text {
-  position: relative;
-  letter-spacing: 0.02em;
-  line-height: 13px;
+  font-family: W95FA, serif;
 }
 .gradientbar {
   height: 100%;
